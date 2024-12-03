1c9a086bf7cade64b41591e242919c839c43fc19
Yeferson77 <114710633+Yeferson77@users.noreply.github.com>
2024-12-03 12:55:06 -0500

    Add files via upload

---
+++ Proyecto Netplus_final/netplus/netplusprj/netplusapp/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+
+AGE_CHOICES = (
+    ('All', 'Adulto'),
+    ('Kids', 'Niño'),
+)
+
+MOVIE_CHOICES = (
+    ('seasonal', 'Temporada'),
+    ('single', 'Unico'),
+)
+
+class CustomUser(AbstractUser):
+    profiles = models.ManyToManyField('Profile', blank = True)
+    
+    
+class Profile(models.Model):
+    name = models.CharField(max_length=1000)
+    age_limit = models.CharField(choices=AGE_CHOICES , max_length=10)
+    uuid = models.UUIDField(default=uuid.uuid4)
+    
+    def __str__(self):
+        return self.name
+    
+class Movie(models.Model):
+    title = models.CharField(max_length=1000)
+    description = models.TextField(blank = True, null=True)
+    created = models.DateTimeField(auto_now_add = True)
+    uuid = models.UUIDField(default=uuid.uuid4)
+    type = models.CharField(choices = MOVIE_CHOICES, max_length=10)
+    video = models.ManyToManyField('Video')
+    image = models.ImageField(upload_to = 'covers')
+    age_limit = models.CharField(choices=AGE_CHOICES , max_length=10)
+    
+    def __str__(self):
+        return self.title
+    
+class Video(models.Model):
+    title = models.CharField(max_length=1000)
+    file = models.FileField(upload_to='movies')
+    def __str__(self):
+        return self.title
+    
+    
+
+
+    
+    
